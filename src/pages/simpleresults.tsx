aef9520fe643444d8017c159f895d75c34a6647c
Madison <mgholt@udel.edu>
2024-11-06 14:29:41 -0500

    adding results pages

---
+++ src/pages/simpleresults.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { Form, Button } from 'react-bootstrap';
+import HeaderComponent from '../components/HeaderComponent';
+
+const basicresults: React.FC = () => {
+return (
+    <div>
+      <HeaderComponent />
+      </div>
+)
+}
+export default basicresults;
