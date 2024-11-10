a8cae535bea7d925a5408b9c421d601df9cac039
shellie <shelliev@udel.edu>
2024-11-09 23:16:34 -0500

    homepage design complete

--- src/components/NavigateDetailedButton.tsx
+++ src/components/NavigateDetailedButton.tsx
@@ -12,7 +12,7 @@ const NavigateDetailedButton: React.FC = () => {
           The detailed career assessment will assess your skills, interests, and preferences with a more nuanced and exploratory approach through a series of creative open-ended questions. By encouraging introspection and creative thinking, this type of test provides deeper insights into your unique talents, passions, and long-term professional goals, offering more personalized career recommendations based on your distinct characteristics.
         </p>
         <Link to="/detailedquestions">
-          <Button style={{ backgroundColor: 'black' }}>
+          <Button className="button-style">
             Detailed Questions Page
           </Button>
         </Link>
