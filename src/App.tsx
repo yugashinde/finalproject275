605461a064409c1b8bb4d77ff7c9b0826432d046
shellie <shelliev@udel.edu>
2024-12-06 01:45:33 -0500

    footer icons

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,20 @@ function App() {
           <Route path="/simpleresults" element={<SimpleResults />} />
         </Routes>
     </Router>
+
+    <div className="footer">
+      <div className="footer-socials">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook" className="social-icon" />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" className="social-icon" />
+        </a>
+        
+      
+      </div>
+    </div>
+
     <Form>
         <Form.Label>API Key:</Form.Label>
 
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -5,15 +5,17 @@ import NavigateDetailedButton from '../components/NavigateDetailedButton';
 import { Row, Col, Container } from 'react-bootstrap';
 import './homepage.css';
 import video from '../video/4782596-uhd_3840_2160_30fps.mp4';
-
+import logo from '../images/shell-vector-icon-logo-illustration-600nw-2151966943.png';
 
 
 const HomePage: React.FC = () => {
   return (
     <div className='homepage'>
       <video className="background-video" src={video} autoPlay loop muted playsInline />
-      <div className="container"></div>
-      <h1 className='homepage-header'>WELCOME TO CAREERCOAST</h1>
+      <div className="container">
+        <img src={logo} alt="CareerCoast Logo" className="logo"/>
+        <h1 className='homepage-header'>WELCOME TO CAREERCOAST</h1>
+        </div>
       <div className = "homepage-description"> Welcome to CareerCoast, your serene pathway to career clarity. Just like the ocean’s waves reveal hidden treasures, our personalized assessments help uncover your unique strengths, guiding you toward a fulfilling career journey. Set sail with us as you explore new horizons, discover opportunities, and chart a course that aligns with your true potential. With CareerCoast, navigating your future is as refreshing as a day at the beach. </div>
       <Container>
       <Row> <Col md={6}> <NavigateSimpleButton /> </Col>
