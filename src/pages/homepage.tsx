d45f4fffe168ace3ba50c29bd57a19dfda521c1f
shellie <shelliev@udel.edu>
2024-12-09 14:02:06 -0500

    homepage

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -7,11 +7,8 @@ import './homepage.css';
 import video from '../video/4782596-uhd_3840_2160_30fps.mp4';
 
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 2d602cec13ad5ad06d0ed097d4cb8ab63758175d
 const HomePage: React.FC = () => {
   return (  
     <div className='homepage'>
