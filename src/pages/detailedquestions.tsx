ac98d834c703a8a87e573d8a1e35f3bec88d4a43
yugandharashinde <shindey@udel.edu>
2024-12-08 21:23:55 -0500

    UI changes

--- src/pages/detailedquestions.tsx
+++ src/pages/detailedquestions.tsx
@@ -14,13 +14,13 @@ const DetailedQuestions: React.FC = () => {
   const navigate = useNavigate();
 
   const [questions, setQuestions] = useState<Question[]>([
-    { id: 1, name: "Imagine you could teach a class on any skill or topic — what would it be, and what makes you the expert?", options: [], answer: "" },
+    { id: 1, name: "What activities or subjects have always captured your interest, and why?", options: [], answer: "" },
     { id: 2, name: "If you were cast in a role for a team-based adventure, what character would you naturally become, and how would you lead the group to success?", options: [], answer: "" },
     { id: 3, name: "You’re given the power to instantly learn a new skill — what would it be?", options: [], answer: "" },
     { id: 4, name: "Describe your ideal workspace: what’s around you, and how does it help you stay in your creative flow?", options: [], answer: "" },
-    { id: 5, name: "If you could spend an entire day doing one activity without getting tired, what would it be, and why?", options: [], answer: "" },
+    { id: 5, name: "What aspects of a job are most important to you: salary, flexibility, creativity, stability, or impact?", options: [], answer: "" },
     { id: 6, name: "Imagine you’re on a deserted island with only one item of your choosing— what is it, and why did you select that tool?", options: [], answer: "" },
-    { id: 7, name: "You’re given the chance to spend a week learning from any professional — who would it be, and what would you hope to gain from the experience?", options: [], answer: "" },
+    { id: 7, name: "How do you define meaningful work?", options: [], answer: "" },
   ]);
  
   const [loading, setLoading] = useState<boolean>(false);
